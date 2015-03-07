9bda56250545f376f0cdf779534c5ccd67bfecc4
DinoL <grech.leonid@gmail.com>
2015-03-07 22:43:25 +0300

    [~] Test nontransitive simulator

--- unitTests.py
+++ unitTests.py
@@ -1,9 +1,21 @@
 from unittest import TestCase
-from Simulators import HardSimulator
-from Simulators import RealisticSimulator
-from Simulators import RandomizedSimulator
+import Simulators
 from Tennis import Player
 
+
+def NonTransitiveSimulatorTest(TestCase):
+    def test_NewbieWins(self):
+        a = Player("a", 0.0)
+        b = Player("b", 0.2)
+        c = Player("c", 0.4)
+        d = Player("d", 1.0)
+        simulator = Simulators.NonTransitiveSimulator()
+        self.assertFalse(simulator.firstPlayerWon(a, b))
+        self.assertFalse(simulator.firstPlayerWon(b, a))
+        self.assertFalse(simulator.firstPlayerWon(b, c))
+        self.assertFalse(simulator.firstPlayerWon(c, b))
+        self.assertFalse(simulator.firstPlayerWon(c, d))
+        self.assertFalse(simulator.firstPlayerWon(d, c))
 
 class HardSimulatorTest(TestCase):
     def test_StrongestWins(self):
@@ -11,7 +23,7 @@ class HardSimulatorTest(TestCase):
         b = Player("b", 0.2)
         c = Player("c", 0.4)
         d = Player("d", 1.0)
-        simulator = HardSimulator()
+        simulator = Simulators.HardSimulator()
         self.assertFalse(simulator.firstPlayerWon(a, b))
         self.assertFalse(simulator.firstPlayerWon(b, c))
         self.assertFalse(simulator.firstPlayerWon(c, d))
@@ -23,14 +35,14 @@ class RealisticSimulatorTest(TestCase):
     def test_TheSameWinsAlways(self):
         a = Player("a")
         b = Player("b")
-        simulator = RealisticSimulator()
+        simulator = Simulators.RealisticSimulator()
         winner = simulator.getWinner(a, b)
         self.assertEqual(simulator.getWinner(a, b), winner)
         self.assertEqual(simulator.getWinner(a, b), winner)
 
 class RandomizedSimulatorTest(TestCase):
     def test_EqualChances(self):
-        simulator = RandomizedSimulator()
+        simulator = Simulators.RandomizedSimulator()
         self.assertEqual(simulator.getFirstVictoryProbability(0.7, 0.7), 0.5)
         self.assertEqual(simulator.getFirstVictoryProbability(0.0, 0.0), 0.5)
         self.assertEqual(simulator.getFirstVictoryProbability(1.0, 1.0), 0.5)
